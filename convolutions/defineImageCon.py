af92ac7f9f91d93401e351c28c552b86d42b7c68
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-05 21:12:05 +0300

    dark spot

--- convolutions/defineImageCon.py
+++ convolutions/defineImageCon.py
@@ -6,3 +6,7 @@ kernel = np.array([[-1, -1, -1],
 kernel = np.array([[-1, -1, -1], 
                    [-1, 1, -1],
                    [-1, -1, -1]])
+##Define a kernel that finds a dark spot surrounded by bright pixels.
+kernel = np.array([[1, 1, 1], 
+                   [1, -1, 1],
+                   [1, 1, 1]])
