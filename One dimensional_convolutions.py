42e50a6a1b5d4a900488c3a07457d28bca754fb0
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-05 21:04:36 +0300

    one dimension

--- One dimensional_convolutions.py
+++ One dimensional_convolutions.py
@@ -4,7 +4,7 @@ conv = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 # Output array
 for ii in range(8):
-    conv[ii] = (____ * array[____:____+____]).sum()
+    conv[ii] = (kernel * array[ii:ii+3]).sum()
 
 # Print conv
 print(conv)
