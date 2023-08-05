8216dbdc88563067f282d6c96c20bf396d94ea28
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-05 21:11:11 +0300

    slight box

--- convolutions/defineImageCon.py
+++ convolutions/defineImageCon.py
@@ -1,3 +1,8 @@
 kernel = np.array([[-1, -1, -1], 
                    [1, 1, 1],
                    [-1, -1, -1]])
+
+##Define a kernel that finds a light spot surrounded by dark pixels.
+kernel = np.array([[-1, -1, -1], 
+                   [-1, 1, -1],
+                   [-1, -1, -1]])
